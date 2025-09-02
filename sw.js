a4d7c0e0fecba9b58dfa3d2587724c51a61b84d6
Qora <lorismziray01@gmail.com>
2025-09-02 18:43:47 +0200

    update

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "DB-V1";
+const BASE = "Deutsche-Bahn-Roblox/";
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        
+        BASE + 'index.html'
+        
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.open(CACHE_NAME).then(cache =>
+    cache.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(fresp => {
+        cache.put(event.request, fresp.clone());
+        return fresp;
+      });
+    })
+  ));
+});
